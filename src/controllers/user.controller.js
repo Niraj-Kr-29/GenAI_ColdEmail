e2d0acc304c4ef72772d94dcb47c26e710a53de4
Niraj-Kr-29 <nkr2348@gmail.com>
2025-07-04 10:29:23 +0530

    added logs in getUserDetails

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,6 +29,8 @@ const updateUserProfile = async(req,res)=>{
 }
 
 const getUserDetails = async(req,res) => {
+    console.log(req.cookies)
+    console.log(req.session)
     try{
         const user = await User.findById(req.user._id)
         res.status(200).json(user)
