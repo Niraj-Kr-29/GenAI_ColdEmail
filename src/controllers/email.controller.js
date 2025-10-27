fc1b8201cdc695038a3d28206ae25d843902e772
Niraj-Kr-29 <nkr2348@gmail.com>
2025-10-28 00:54:30 +0530

    Gemini model change from 1.5 to 2.5

--- src/controllers/email.controller.js
+++ src/controllers/email.controller.js
@@ -18,7 +18,7 @@ const generateEmail = async (req, res) => {
   }
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const prompt = `
             You are an expert email copywriter.
 
